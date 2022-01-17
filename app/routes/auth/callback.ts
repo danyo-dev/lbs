b36a400211a6807d12e4f0f13cdfd5f55c7dd555
Michael Kaner <Michael.Kaner@oebb.at>
2022-01-17 15:31:15 +0100

    changed "let" to "const" and removed unused Type

--- app/routes/auth/callback.ts
+++ app/routes/auth/callback.ts
@@ -2,7 +2,7 @@ import type { LoaderFunction } from "remix";
 
 import { authenticator } from "~/services/auth.server";
 
-export let loader: LoaderFunction = ({ request }) => {
+export const loader: LoaderFunction = ({ request }) => {
   return authenticator.authenticate("auth0", request, {
     successRedirect: "/admin/overview",
     failureRedirect: "/",
--- app/routes/auth/login.ts
+++ app/routes/auth/login.ts
@@ -1,8 +1,8 @@
-import type { ActionFunction, LoaderFunction } from "remix";
+import type { ActionFunction } from "remix";
 
 import { authenticator } from "~/services/auth.server";
 
-export let action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({ request }) => {
   return authenticator.authenticate("auth0", request, {
     successRedirect: "/admin/overview",
   });
