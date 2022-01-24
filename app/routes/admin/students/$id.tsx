cff2c5c12e371d9ae4ee61ff001865e5b327de56
Michael Kaner <Michael.Kaner@oebb.at>
2022-01-24 11:48:46 +0100

    pr update

--- app/routes/admin/students/$id.tsx
+++ app/routes/admin/students/$id.tsx
@@ -190,15 +190,6 @@ export default function EditStudent() {
           </div>
         </div>
       </form>
-      <Form action="/auth/brzRefresh" method="post">
-        <input type="hidden" name="origin" value={pathName} />
-        <button
-          type="submit"
-          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Refresh Token
-        </button>
-      </Form>
     </>
   );
 }
