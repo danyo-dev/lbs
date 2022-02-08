17320c30cddf9949ae401f87687eed7b7eb6392e
Michael Kaner <Michael.Kaner@oebb.at>
2022-02-08 13:04:23 +0100

    added: catchboundry for multiple errors in response

--- app/routes/admin/students/$studentId/general.tsx
+++ app/routes/admin/students/$studentId/general.tsx
@@ -1,18 +1,13 @@
 import { useCatch, useFetcher, useMatches, useParams } from "remix";
 import BrzGeneralDataBox from "~/components/BrzGeneralDataBox";
-import BrzGeneralDataForm from "~/components/BrzGeneralDataForm";
-import { withFetcherLoader } from "~/components/hoc/withFetcherLoader";
+
+import BrzGetStammdatenForm from "~/components/BrzGetStammdatenForm";
 import { StudentProfile } from "~/types/responseTypes";
 
 export default function StudentGeneralRoute() {
   const fetcherData = useFetcher();
 
   const { state, type, data, Form } = fetcherData;
-
-  const BrzGeneralDataBoxWithLoader = withFetcherLoader(
-    BrzGeneralDataBox,
-    type
-  );
 
   const params = useParams();
   const studentData = useMatches().find(
@@ -24,16 +19,17 @@ export default function StudentGeneralRoute() {
   );
 
   return (
-    <div className="w-3/4 my-12">
-      <section className=" border-slate-200 gap-6 ">
-        <BrzGeneralDataForm Form={Form} state={state} student={student} />
+    <div className="w-full my-12 grid grid-cols-12 gap-6">
+      <section className=" border-slate-200 col-span-6">
+        <h2 className="text-xl text-slate-600 mb-2 ml-2">
+          BRZ Stammdaten Abfragen
+        </h2>
+        <BrzGetStammdatenForm Form={Form} state={state} student={student} />
       </section>
 
-      <section className="border-slate-200 gap-6 mt-6">
-        <h2 className="text-xl text-slate-600 mt-6 mb-2 ml-2">
-          BRZ Stammdaten
-        </h2>
-        <BrzGeneralDataBoxWithLoader data={data} />
+      <section className="border-slate-200 col-span-6 ">
+        <h2 className="text-xl text-slate-600 mb-2 ml-2">BRZ Stammdaten</h2>
+        <BrzGeneralDataBox data={data} type={type} />
       </section>
     </div>
   );
@@ -41,16 +37,23 @@ export default function StudentGeneralRoute() {
 export function CatchBoundary() {
   const caught = useCatch();
   const parseData = JSON.parse(caught.data);
+  const errors = parseData.FehlerAntwort?.fehlerliste?.fehler;
 
-  const errorMsg =
-    parseData.FehlerAntwort?.fehlerliste?.fehler?.massnahme?._text;
+  const isMultipleErrors = Array.isArray(errors);
 
   return (
     <div className="error-container">
       <div className="text-2xl font-bold mb-2">
         Error: Status {caught.status}
       </div>
-      <div className="text-slate-700 ">{errorMsg}</div>
+      {isMultipleErrors ? (
+        errors.map((e) => {
+          console.log(e);
+          return <div className="text-slate-700 ">{e.fehlertext._text}</div>;
+        })
+      ) : (
+        <div>{errors.fehlertext._text}</div>
+      )}
     </div>
   );
 }
