e944f23e15e0403b2b9846d105f706c7ec0f4305
Daniel Holpfer <daniel.holpfer@me.com>
2022-02-25 19:10:43 +0100

    update tailwind config to support grid-areas; update .env.example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
+    gridTemplateAreas: {
+      'overview': [
+        '. form',
+        '. form',
+        '. form',
+      ],
+    },
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require('@savvywombat/tailwindcss-grid-areas')],
 };
