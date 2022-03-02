8000f94889044734cdd63fea0c58b30c0a832778
Michael Kaner <Michael.Kaner@oebb.at>
2022-03-02 16:20:27 +0100

    added: addditional text to boxes

--- app/routes/admin/dashboard/index.tsx
+++ app/routes/admin/dashboard/index.tsx
@@ -4,30 +4,45 @@ export default function DashboardIndexRoute() {
   return (
     <>
       <section>
-        <h2 className="text-3xl font-bold text-gray-900 mb-6">BRZ Abfragen</h2>
+        <h2 className="text-3xl font-bold text-slate-900 mb-6">BRZ Abfragen</h2>
         <ul className="grid grid-cols-3 gap-8">
           <li>
             <NavLink
               to="matrikel"
-              className="h-40 bg-white flex justify-center items-center rounded-lg px-2 text-xl hover:shadow "
+              className="h-40 bg-white flex flex-col justify-center items-center rounded-lg px-2 text-xl shadow hover:shadow-md "
             >
-              Matrikel
+              <h3 className="text-xl font-bold text-slate-600 mb-2">
+                Matrikel
+              </h3>
+              <div className="text-sm text-slate-400">
+                Matrikelabfrage vom BRZ DV vornehmen
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink
               to="stammdaten"
-              className=" h-40 bg-white flex justify-center items-center rounded-lg text-xl hover:shadow"
+              className=" h-40 bg-white flex flex-col justify-center items-center rounded-lg text-xl shadow hover:shadow-md"
             >
-              Stammdaten
+              <h3 className="text-xl font-bold text-slate-600 mb-2">
+                Stammdaten
+              </h3>
+              <div className="text-sm text-slate-400">
+                Stammdatenabfrage vom BRZ DV vornehmen
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink
               to="studiendaten"
-              className=" h-40 bg-white flex justify-center items-center rounded-lg text-xl hover:shadow"
+              className=" h-40 bg-white flex flex-col justify-center items-center rounded-lg text-xl shadow hover:shadow-md"
             >
-              Studiendaten
+              <h3 className="text-xl font-bold text-slate-600 mb-2">
+                Studiendaten
+              </h3>
+              <div className="text-sm text-slate-400">
+                Studiendatenabfrage vom BRZ DV vornehmen
+              </div>
             </NavLink>
           </li>
         </ul>
